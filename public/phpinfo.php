a577c0015181b2c20d7a9ec5db6d4118ce229739
Douglas Medeiros <medeirosinacio@outlook.com>
2020-05-29 00:01:43 -0300

    set phpinfo only local dev

--- public/phpinfo.php
+++ public/phpinfo.php
@@ -1,3 +1,16 @@
 <?php
+
+use Dotenv\Dotenv;
+
 require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__ . "/../docker");
+$dotenv->load();
+
+if (env('APP_ENV') != 'local') {
+    header('HTTP/2 404 Not Found'); //This may be put inside err.php instead
+    $_GET['e'] = 404; //Set the variable for the error code (you cannot have a
+    exit; //Do not do any more work in this script.
+}
+
 phpinfo();
