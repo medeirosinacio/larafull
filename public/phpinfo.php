a6e711f6d5b50b6a517c8b948b2a3351c41f465d
Douglas Medeiros <medeirosinacio@outlook.com>
2020-05-29 08:56:34 -0300

    valide env local

--- public/phpinfo.php
+++ public/phpinfo.php
@@ -8,9 +8,9 @@ $dotenv = Dotenv::createImmutable(__DIR__ . "/../docker");
 $dotenv->load();
 
 if (env('APP_ENV') != 'local') {
-    header('HTTP/2 404 Not Found'); //This may be put inside err.php instead
-    $_GET['e'] = 404; //Set the variable for the error code (you cannot have a
-    exit; //Do not do any more work in this script.
+    header('HTTP/2 404 Not Found');
+    $_GET['e'] = 404;
+    exit;
 }
 
 phpinfo();
