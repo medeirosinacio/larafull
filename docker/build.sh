7c9429b23eb6243691a96c33f315b0aab3fac4a0
Douglas Medeiros <medeirosinacio@outlook.com>
2020-07-03 18:22:29 -0300

    add pgadmin, redisadmin e configure nginx env sub

--- docker/build.sh
+++ docker/build.sh
@@ -3,8 +3,11 @@
 docker-compose up -d --force-recreate --build --remove-orphans
 
 # COMPOSER UPDATE
-#docker run --rm --tty \
-#  --volume $PWD/../:/app \
-#  --volume $HOME/.composer:/tmp \
-#  --user $(id -u):$(id -g) \
-#  composer update
+ docker run --rm --tty \
+  --volume $PWD/../:/app \
+  --volume $HOME/.composer:/tmp \
+  --user $(id -u):$(id -g) \
+  composer update
+
+docker exec -it laravel-phpfpm php bin/artisan config:cache
+docker exec -it laravel-phpfpm php bin/artisan config:clear
